cfeed5264c153f5a2771f5a7316ac9d917f06301
lee-jongbae78 <leejongbae78@gmail.com>
2020-07-13 16:21:24 +0900

    0713

---
+++ connectclock.js
@@ -0,0 +1,17 @@
+const connectedDate = new Date();
+
+
+function showClock(){
+    const currentDate = new Date();
+    const divClock = document.getElementById("divClock");
+        
+    const result = Math.ceil((currentDate - connectedDate)/1000);
+
+        
+    divClock.innerText = "운동 시간  "+ result;
+    divClock.innerText +="초가 지났습니다.";
+        
+    setTimeout(showClock,1000);
+}
+
+//showClock();
