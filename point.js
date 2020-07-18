836e607bf3fee59813b1315286b9895e999484ea
lee-jongbae78 <leejongbae78@gmail.com>
2020-07-19 00:53:57 +0900

    0719

--- point.js
+++ point.js
@@ -2,17 +2,26 @@ const point = document.querySelector(".point");
 
 const STRANGTH_01 = "strangth";
 
+function askForPoint(){
+    const STRANGTH_01 = localStorage.setItem("strangth", 10);
+}
+
+function paintpoint(text){
+    point.innerText = `체력이 ${text} 점` + '  올랐습니다.' ;
+}
+
 function getpoint(){
     const strangth = localStorage.getItem(STRANGTH_01);
     if(strangth === null){
-
+        askForPoint();
     } else {
-        
+        paintpoint(localStorage.getItem("strangth"));
     }
 
 }
 
 function init(){
+    localStorage.setItem("strangth", 50);
     getpoint();
 
 }
